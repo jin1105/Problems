c80a4b2ec392c23155bdef33f0cce4c760ac6c7c
jin1105 <670522325@qq.com>
2017-05-25 10:31:40 +0800

    isValidBST

---
+++ src/MinDepth.java
@@ -0,0 +1,12 @@
+
+public class MinDepth {
+	public static void main(String[] args){
+		System.out.print("000000");
+	}
+	public static int minDepth(TreeNode root) {
+		if (root == null)return 0;
+		
+		return 0;
+        
+    }
+}
